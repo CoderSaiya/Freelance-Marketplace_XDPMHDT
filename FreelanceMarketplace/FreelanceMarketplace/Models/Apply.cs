2a37f56d624737f38ac5e1f0075f5035a7fc79d8
Tien Vo <vominh11022004@gmail.com>
2024-09-30 22:15:26 +0700

    added model Apply and Img

---
+++ FreelanceMarketplace/FreelanceMarketplace/Models/Apply.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace FreelanceMarketplace.Models
+{
+    public class Apply
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public int ProjectId { get; set; }
+        [Required]
+        public int FreelancerId { get; set; }  
+        [Required]
+        [MaxLength(500)]
+        public string CoverLetter { get; set; }  
+
+        [Required]
+        public DateTime ApplyDate { get; set; }  
+
+        public bool IsAccepted { get; set; }  
+
+        public DateTime? DecisionDate { get; set; }  
+
+        
+        public string Remarks { get; set; }  
+    }
+}
---
+++ FreelanceMarketplace/FreelanceMarketplace/Models/Img.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System;
+
+
+namespace FreelanceMarketplace.Models
+{
+    public class Img
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(255)]
+        public string ImageName { get; set; }  
+
+        [Required]
+        [MaxLength(2048)]
+        public string ImageUrl { get; set; }  
+
+        [MaxLength(255)]
+        public string MimeType { get; set; }  
+
+        [Required]
+        public DateTime UploadedDate { get; set; }  
+
+        public long FileSize { get; set; }  
+
+        
+        public int? ProductId { get; set; }  
+        public string UploadedBy { get; set; }  
+    }
+}
