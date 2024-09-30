d81199fc2d1806c6ac7f5b44ef7b44f424f76615
Phuc Hau <haunhpr024@gmail.com>
2024-09-30 20:40:08 +0700

    added models UserProfile and Notification

---
+++ FreelanceMarketplace/FreelanceMarketplace/Models/Notification.cs
@@ -0,0 +1,12 @@
+﻿namespace FreelanceMarketplace.Models
+{
+    public class Notification
+    {
+        public int Id { get; set; }
+        public string? Message { get; set; }
+        public bool? IsRead { get; set; } = false;
+        public DateTime? Created { get; set; }
+
+        public ICollection<Users>? User { get; set; }
+    }
+}
