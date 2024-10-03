ad6b272080392e3e27e7817e637c83e584b1960c
thienhuyz <phamhuynhthienhuy001@gmail.com>
2024-10-03 21:08:51 +0700

    Update #1: Add interface and service for basic CRUD of Category Model

---
+++ FreelanceMarketplace/FreelanceMarketplace/Services/Interface/ICategoryService.cs
@@ -0,0 +1,13 @@
+﻿using FreelanceMarketplace.Models;
+
+namespace FreelanceMarketplace.Services.Interface
+{
+    public interface ICategoryService
+    {
+        Task<List<Category>> GetAllCategoriesAsync();
+        Task<Category?> GetCategoryByIdAsync(int categoryId);
+        Task<Category> CreateCategoryAsync(Category category);
+        Task UpdateCategoryAsync(Category category);
+        Task DeleteCategoryAsync(int categoryId);
+    }
+}
