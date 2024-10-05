0c3d45c8cecaf80e7827c676e5d32c96ba31572b
Thai Binh <pntb2004@gmail.com>
2024-10-05 17:11:43 +0700

    add review and payment service

---
+++ FreelanceMarketplace/FreelanceMarketplace/Services/PaymentService.cs
@@ -0,0 +1,106 @@
+﻿using FreelanceMarketplace.Data;
+using FreelanceMarketplace.Models;
+using FreelanceMarketplace.Services.Interface;
+using Microsoft.EntityFrameworkCore;
+
+namespace FreelanceMarketplace.Services
+
+{
+    public class PaymentService : IPaymentService
+    {
+        private readonly AppDbContext _context;
+
+        public PaymentService(AppDbContext context)
+        {
+            _context = context;
+        }
+
+            public async Task<Payment> CreatePaymentAsync(Payment payment)
+        {
+            try
+            {
+                _context.Payments.Add(payment);
+                await _context.SaveChangesAsync();
+                return payment;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error creating payment", ex);
+            }
+        }
+
+        public async Task DetetePaymentAsync(int paymentId)
+        {
+            try
+            {
+                var payment = await _context.Payments.FindAsync(paymentId);
+                if (payment == null)
+                {
+                    throw new KeyNotFoundException("Payment not found");
+                }
+                _context.Payments.Remove(payment);
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error deleting payment", ex);
+            }
+        }
+
+        public async Task<List<Payment>> GetAllPaymentsAsync()
+        {
+            try
+            {
+                return await _context.Payments
+                    .Include(p =>p.Contract)
+                    .ToListAsync();
+            }
+            catch(Exception ex)
+            {
+                throw new Exception("Error retrieving payments ", ex);
+            }
+        }
+
+        public async Task<Payment> GetPaymentByIdAsync(int paymentId)
+        {
+            try
+            {
+                var payment = await _context.Payments
+                    .Include (p =>p.Contract)
+                    .FirstOrDefaultAsync(p=>p.PaymentId== paymentId);
+
+                if (payment == null)
+                    throw new KeyNotFoundException("Payment not found");
+                return payment;
+            }
+            catch(Exception ex)
+            {
+                throw new Exception($"Error retrieving payment with ID {paymentId}", ex);
+
+            }
+
+        }
+
+        public async Task UpdatePaymentAsync(Payment payment)
+        {
+            try
+            {
+                var existingPayment = await _context.Payments.FindAsync(payment.PaymentId);
+                if (existingPayment == null)
+                    throw new KeyNotFoundException("Paymant not found");
+
+                existingPayment.ContractId= payment.ContractId;
+                existingPayment.Amount = payment.Amount;
+                existingPayment.Description= payment.Description;
+                existingPayment.Status= payment.Status;
+
+                _context.Payments.Update(existingPayment);
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error updating payment", ex);
+            }
+        }
+    }
+}
---
+++ FreelanceMarketplace/FreelanceMarketplace/Services/ReviewService.cs
@@ -0,0 +1,111 @@
+﻿using FreelanceMarketplace.Data;
+using FreelanceMarketplace.Models;
+using FreelanceMarketplace.Services.Interface;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace FreelanceMarketplace.Services
+{
+    public class ReviewService : IReviewService
+    {
+        private readonly AppDbContext _context;
+
+            public ReviewService(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Review>> GetAllReviewsAsync()
+        {
+            try
+            {
+                return await _context.Reviews
+                    .Include(c => c.User)
+                    .Include(c =>c.Contract)
+                    .ToListAsync();
+            }
+            catch(Exception ex)
+            {
+                throw new Exception("Error retrieving reviews", ex);
+
+            }
+        }
+        
+        public async Task<Review> GetReviewByIdAsync(int reviewId)
+        {
+            try
+            {
+                var review = await _context.Reviews
+                    .Include(c => c.User)
+                    .Include(c => c.Contract)
+                    .FirstOrDefaultAsync();
+
+                if (review == null)
+                    throw new KeyNotFoundException("Review not found");
+                return review;
+            }
+            catch(Exception ex)
+            {
+                throw new Exception($"Error retrieving review with ID {reviewId}", ex);
+            }
+
+        }
+
+        public async Task<Review> CreateReviewAsync(Review review)
+        {
+            try
+            {
+                _context.Reviews.Add(review);
+                await _context.SaveChangesAsync();
+                return review;
+            }
+            catch(Exception ex)
+            {
+                throw new Exception("Error creating review", ex);
+            }
+        }
+
+        public async Task UpdateReviewAsync(Review review)
+        {
+            try
+            {
+                var existingReview = await _context.Reviews.FindAsync(review.ReviewId);
+                if (existingReview == null)
+                {
+                    throw new KeyNotFoundException("Review not found");
+                }
+                
+                existingReview.UserId= review.UserId;
+                existingReview.ContractId= review.ContractId;
+                existingReview.Rating= review.Rating;
+                existingReview.Feedback= review.Feedback;
+                
+                _context.Reviews.Update(existingReview);
+                await _context.SaveChangesAsync();
+            }
+            catch(Exception ex)
+            {
+                throw new Exception("Error updating review", ex);
+            }
+        }
+
+        public async Task  DeleteReviewAsync(int reviewId)
+        {
+            try
+            {
+                var review = await _context.Reviews.FindAsync(reviewId);
+                    if (review == null)
+                {
+                    throw new KeyNotFoundException("Review not found");
+                }
+                    _context.Reviews.Remove(review);
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error deleting review", ex);
+            }
+        }
+        
+    }
+}
