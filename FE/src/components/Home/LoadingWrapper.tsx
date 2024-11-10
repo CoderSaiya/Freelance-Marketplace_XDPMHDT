46b9ede4a23e9607aeaabde925f861090e7304df
Nhật Cường <119063991+CoderSaiya@users.noreply.github.com>
2024-11-10 11:35:33 +0700

    add exclude path in loading wrapper

--- FE/src/components/Home/LoadingWrapper.tsx
+++ FE/src/components/Home/LoadingWrapper.tsx
@@ -2,20 +2,24 @@ import { useLocation } from "react-router-dom";
 import Loading from "./Loading";
 import { useEffect, useState } from "react";
 
-const LoadingWrapper: React.FC<React.PropsWithChildren> = ({
-  children,
-}) => {
+const LoadingWrapper: React.FC<React.PropsWithChildren> = ({ children }) => {
   const [isLoading, setIsLoading] = useState(false);
   const location = useLocation();
 
-  useEffect(() => {
-    setIsLoading(true);
-    
-    const timeoutId = setTimeout(() => {
-      setIsLoading(false);
-    }, 2000);
+  const excludedPaths = ["/dashboard"];
 
-    return () => clearTimeout(timeoutId);
+  useEffect(() => {
+    if (!excludedPaths.includes(location.pathname)) {
+      setIsLoading(true);
+
+      const timeoutId = setTimeout(() => {
+        setIsLoading(false);
+      }, 2000);
+
+      return () => clearTimeout(timeoutId);
+    } else {
+      setIsLoading(false);
+    }
   }, [location]);
 
   return (
